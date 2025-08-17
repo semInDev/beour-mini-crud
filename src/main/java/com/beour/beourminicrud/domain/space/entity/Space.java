d1361acb4b0fda90730f8a4a227f0ea21fa3cc5a
sem_in_github <semin6634@naver.com>
2025-08-17 16:48:41 +0900

    [#2] feat: Space Entity 생성

---
+++ src/main/java/com/beour/beourminicrud/domain/space/entity/Space.java
@@ -0,0 +1,25 @@
+package com.beour.beourminicrud.domain.space.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Space {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private String description;
+}
