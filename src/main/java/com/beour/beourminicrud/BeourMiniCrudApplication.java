3b260df9a44afdc0b99274954dac54dbcc3f063a
sem_in_github <semin6634@naver.com>
2025-08-17 16:31:27 +0900

    [#1] init: Spring Boot 프로젝트 생성 및 기본 설정
    - Gradle 기반 Spring Boot 프로젝트 생성
    - Dependencies 추가: Spring Web, Spring Data JPA, MySQL Driver, Lombok
    - src/main/resources/application.yml 생성 및 DB 연결 기본 설정 추가

---
+++ src/main/java/com/beour/beourminicrud/BeourMiniCrudApplication.java
@@ -0,0 +1,13 @@
+package com.beour.beourminicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeourMiniCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeourMiniCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beour/beourminicrud/BeourMiniCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beour.beourminicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeourMiniCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
