a8b7dfdc8ada1b318c48286cea5cfcd9e729aab5
sem_in_github <semin6634@naver.com>
2025-08-25 14:41:14 +0900

    [#6] refactor: dto, entity 변환 로직 위치 변경 및 spaceCategory 필드 추가

--- src/main/java/com/beour/beourminicrud/space/dto/SpaceResponseDto.java
+++ src/main/java/com/beour/beourminicrud/space/dto/SpaceResponseDto.java
@@ -1,5 +1,6 @@
 package com.beour.beourminicrud.space.dto;
 
+import com.beour.beourminicrud.space.entity.Space;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,6 +9,17 @@ import lombok.Getter;
 public class SpaceResponseDto {
     private Long id;
     private String name;
+    private String spaceCategory;
     private String address;
     private String description;
+
+    public static SpaceResponseDto fromEntity(Space space) {
+        return new SpaceResponseDto(
+                space.getId(),
+                space.getName(),
+                space.getSpaceCategory().getText(),
+                space.getAddress(),
+                space.getDescription()
+        );
+    }
 }
