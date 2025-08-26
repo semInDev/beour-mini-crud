dd30f9b6da663c3728e76f484439a09c0e69b145
sem_in_github <semin6634@naver.com>
2025-08-26 11:21:23 +0900

    [#8] feat: Space 엔티티에 nullable 속성 추가
    - DB 무결성 보장을 위해

--- src/main/java/com/beour/beourminicrud/space/entity/Space.java
+++ src/main/java/com/beour/beourminicrud/space/entity/Space.java
@@ -18,11 +18,17 @@ public class Space {
     private Long id;
 
     // private Long hostId;
+    @Column(nullable = false) // DTO에서도 체크하긴 하지만, Entity에도 설정해주면 안정성 있고, DB 무결성 보장 가능!
     private String name;
 
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING) // 이거 없으면 DB에 ordinal(숫자) 값으로 저장됨. enum 순서 바꾸면 데이터 꼬임
     private SpaceCategory spaceCategory;
+
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
     private String description;
 
     public void update(String name, SpaceCategory spaceCategory, String address, String description) {
